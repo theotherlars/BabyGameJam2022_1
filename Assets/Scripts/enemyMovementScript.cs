84a95faa2ba54aa124c2be0dc94a2007702206a9
Lasse B. I <lassebi92@gmail.com>
2022-04-29 21:51:12 +0200

    Enemies constantly turn rotation towards player

--- Assets/Scripts/enemyMovementScript.cs
+++ Assets/Scripts/enemyMovementScript.cs
@@ -15,17 +15,36 @@ public class enemyMovementScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         Vector2 currentPos = transform.position;
+        Vector2 currentPos = transform.position;
+
+        // enemy flies forward in a straigth line
         // transform.position = currentPos + moveDirection * Time.deltaTime; 
 
-
+        // enemy flies toward specified gameObject
         transform.position = Vector2.MoveTowards(currentPos, PointToMoveTowards.transform.position, moveSpeed * Time.deltaTime);
+        // constantly change facing direction
+        RotateTowardsPlayer();
 
+
+        //transform.rotation.SetLookRotation(PointToMoveTowards.transform.position);
+
+
+
+
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        float offset = 270;
+        Vector2 direction = PointToMoveTowards.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
     }
 }
