ab28b03d54d3f859ada0c783c4859200ee0bbb87
Lasse B. I <lassebi92@gmail.com>
2022-04-29 18:56:53 +0200

    Player rotation functionalish

---
+++ Assets/Scripts/enemyMovementScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovementScript : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Vector3 moveDirection;
+
+    public GameObject PointToMoveTowards;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector2 currentPos = transform.position;
+        // transform.position = currentPos + moveDirection * Time.deltaTime;
+
+
+        transform.position = Vector2.MoveTowards(currentPos, PointToMoveTowards.transform.position, moveSpeed * Time.deltaTime);
+
+    }
+}
