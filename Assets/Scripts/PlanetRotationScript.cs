a9826e12f02b02a86847d2cdb743e4d9e333f680
Lasse B. I <lassebi92@gmail.com>
2022-04-29 18:55:59 +0200

    Player rotation functional

---
+++ Assets/Scripts/PlanetRotationScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotationScript : MonoBehaviour
+{
+
+
+    public float playerTurnSpeed;
+
+    void Update()
+    {
+        PlayerRotate();
+    }
+
+    private void PlayerRotate()
+    {
+        Vector3 currentPos = transform.eulerAngles;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0)
+        {
+            transform.eulerAngles = currentPos + new Vector3(0, 0, -playerTurnSpeed) * Time.deltaTime;
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.eulerAngles = currentPos + new Vector3(0, 0, playerTurnSpeed) * Time.deltaTime;
+        }
+    }
+}
