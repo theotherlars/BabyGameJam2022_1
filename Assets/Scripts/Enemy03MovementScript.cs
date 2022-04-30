a09f03fc444f0207c7837bd4c1fe5d1a7e571daa
Lasse B. I <lassebi92@gmail.com>
2022-04-30 18:56:41 +0200

    CATHULLO IMPLIMENTED!

---
+++ Assets/Scripts/Enemy03MovementScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy03MovementScript : MonoBehaviour
+{
+
+
+
+    [SerializeField] float speed;
+    [SerializeField] float subtractionDistance;
+    [SerializeField] float timeBetweenSubtractingDistance;
+    [SerializeField]Vector3 pointA;
+    [SerializeField]Vector3 pointB;
+
+    void Start()
+    {
+        StartCoroutine("MoveDownAfterCountDown");
+    }
+
+    void Update()
+    {
+        //PingPong between 0 and 1
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+        
+        
+
+
+        //Invoke("MoveDown", 1);
+    }
+
+
+    IEnumerator MoveDownAfterCountDown()
+    {
+        yield return new WaitForSeconds(timeBetweenSubtractingDistance);
+        Debug.Log("Pew pew");
+        pointA.y = pointA.y -1;
+        pointB.y = pointB.y -1;
+        StartCoroutine("MoveDownAfterCountDown");
+
+    }
+
+
+
+
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
-public class PlayerManager : Health {
-    
-    private void Awake() {
+
+public class PlayerManager : Health
+{
+
+
+    public int IngameCurrency;  // currency used to purchase upgrades
+
+    private void Awake()
+    {
         currentHealth = maxHealth;
     }
 
-    public void Shake(){
+    public void Shake()
+    {
         CameraShakeScript.Instance.CameraShake();
     }
 
-    public void PlayerDied(){
-        
-    }   
+    public void PlayerDied()
+    {
+
+    }
+
+    public void IncreaseCurrencyCount(int amount)
+    {
+        IngameCurrency = IngameCurrency + amount;
+    }
 }
