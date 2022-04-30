d7409c3a359b93b3cc4aa218a83df8d57f9007ce
Lars Gøran Hansen <larsern90@gmail.com>
2022-04-30 13:40:51 +0200

    Added minibombs

---
+++ Assets/Scripts/Projectiles/MiniBomb.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MiniBomb : Projectile2
+{
+    [SerializeField] float timeBeforeExplode;
+    [SerializeField] float explosionRadius;
+    [SerializeField] Light2D light2D; 
+    [SerializeField] Color redLight;
+    [SerializeField] Color greenLight;
+    [SerializeField] int blinkCount;
+    float timeSinceBlink; 
+    float timeSinceSpawn;
+    int blinks;
+
+    private void Start() {
+        light2D.color = greenLight;
+    }
+
+    private void Update() {
+        if(timeSinceSpawn > timeBeforeExplode){
+            Explode();
+        }
+        if(timeSinceBlink > (timeBeforeExplode / blinkCount) && blinks <= blinkCount){
+            if(blinks == blinkCount -2){
+                light2D.color = redLight;
+            } 
+            StartCoroutine(Blink(timeBeforeExplode / blinkCount));
+            blinks++;
+            timeSinceBlink = 0;
+        }
+        
+        timeSinceBlink += Time.deltaTime;
+        timeSinceSpawn += Time.deltaTime;
+    }
+
+    IEnumerator Blink(float duration)
+    {print("here");
+        light2D.intensity = 0;
+        float timer = 0;
+        while(timer < duration / 2){
+            light2D.intensity = Mathf.Lerp(light2D.intensity,3,duration/2 * Time.deltaTime);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        timer = 0;
+        while(timer < duration / 2){
+            light2D.intensity = Mathf.Lerp(light2D.intensity,0,duration/2 * Time.deltaTime);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        light2D.intensity = 0;
+    }
+    private void Explode(){
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position,explosionRadius,Vector2.zero);
+        if(hits.Length > 0){
+            foreach(var hit in hits){
+                if(hit.transform.TryGetComponent(out EnemyBase enemy)){
+                    DealDamage(enemy);
+                    onHit.Invoke();
+                }
+            }
+        }
+        Instantiate(explosion,transform.position,Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Projectiles/Projectile2.cs
+++ Assets/Scripts/Projectiles/Projectile2.cs
@@ -1,39 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile2 : ProjectileBase
 {
-    [SerializeField]float frequency;
-    [SerializeField]float magnitude = 0.5f;
+    [SerializeField] float frequency;
+    [SerializeField] float magnitude = 0.5f;
+    [SerializeField] float rotationSpeed = 1.0f;
+    [SerializeField] Vector3 from = new Vector3(0f, 0f, 45f);
+    [SerializeField] Vector3 to = new Vector3(0f, 0f, -45f);
+    [SerializeField] Transform childPos;
+    [SerializeField] GameObject miniBomb;
+    [SerializeField] int numberOfBombsToSpawn;
+    [SerializeField] float spawnAllBombsWithinTime;
     Vector3 direction;
-    Vector3 pos;
-    Vector2 axis;
+    float timerSinceLastBomb;
+    int spawnedBombs = 0;
 
     public override void Init(){
         base.Init();
-        pos = transform.position;
-        axis = transform.up;
+        Destroy(gameObject,timeBeforeDestroy);
     }
 
     private void Start() {
         Init();
+        timerSinceLastBomb = (spawnAllBombsWithinTime / numberOfBombsToSpawn) / 2;
+    }
+
+    private void Update() {
+        
+        if(timerSinceLastBomb > spawnAllBombsWithinTime / numberOfBombsToSpawn && spawnedBombs < numberOfBombsToSpawn){
+            Instantiate(miniBomb,childPos.position,Quaternion.identity);
+            timerSinceLastBomb = 0;
+            spawnedBombs++;
+        }
+        timerSinceLastBomb += Time.deltaTime;
     }
 
     private void FixedUpdate() {
-        Move();
+        if(allowMovement){
+            Move();
+        }
     }
 
-    private void Move(){
-        Vector2 newVelocity;
-        newVelocity.x = movementSpeed;
-        newVelocity.y = magnitude * frequency * Mathf.Cos(Time.time * frequency);
-        rb.velocity = newVelocity;
-
-        // Vector2 tempPos;
-        // tempPos.x = movementSpeed;
-        // tempPos.y = magnitude * frequency * Mathf.Cos(Time.time * frequency);
-        // transform.position = tempPos;
+    private void Move()
+    {   direction = transform.up;
+        rb.velocity = direction * movementSpeed;
+        WaveMove();
+        Rotate();
     }
 
+    private void WaveMove(){
+        Vector2 tempPos = childPos.localPosition;
+        tempPos.x = Mathf.Sin(Time.time * frequency) * magnitude;
+        childPos.localPosition = tempPos;
+    }
+
+    private void Rotate(){
+        float t = (Mathf.Sin (Time.time * rotationSpeed * Mathf.PI * 2.0f) + 1.0f) / 2.0f;
+        childPos.localEulerAngles = Vector3.Lerp (from, to, t);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Enemy")){
+            if(other.gameObject.TryGetComponent(out EnemyBase enemy)){
+                DealDamage(enemy);
+                onHit.Invoke();
+                Instantiate(explosion,transform.position,Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
